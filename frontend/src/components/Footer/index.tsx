68dc2cd63b988fa1892369d2eff687e813aa09e8
lipengzhu1996 <lipeng_zhu@outlook.com>
2022-12-02 16:46:49 -0800

    update nav bar and footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,3 +1,78 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+
+const styles = {
+  footerWrapper: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "90%",
+    margin: "0 auto",
+  },
+};
+
+const Item = styled(Paper)(({ theme }) => ({
+  background: "none",
+  padding: theme.spacing(1),
+  textAlign: "center",
+}));
+
 export default function Footer() {
-  return <div />;
+  return (
+    <Container maxWidth="lg">
+      <Toolbar disableGutters style={styles.footerWrapper}>
+        <Typography
+          color="textSecondary"
+          component="p"
+          variant="caption"
+          gutterBottom={false}
+        >
+          © 2022 BlockCarbon All rights reserved.
+        </Typography>
+        <Box>
+          <Grid container spacing={0}>
+            <Grid item xs="auto">
+              <Item elevation={0}>
+                <IconButton aria-label="Example">
+                  <EmailIcon />
+                </IconButton>
+              </Item>
+            </Grid>
+            <Grid item xs="auto">
+              <Item elevation={0}>
+                <IconButton aria-label="Example">
+                  <FacebookIcon />
+                </IconButton>
+              </Item>
+            </Grid>
+            <Grid item xs="auto">
+              <Item elevation={0}>
+                <IconButton aria-label="Example">
+                  <InstagramIcon />
+                </IconButton>
+              </Item>
+            </Grid>
+            <Grid item xs="auto">
+              <Item elevation={0}>
+                <IconButton aria-label="Example">
+                  <GitHubIcon />
+                </IconButton>
+              </Item>
+            </Grid>
+          </Grid>
+        </Box>
+      </Toolbar>
+    </Container>
+  );
 }
--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -1,17 +1,16 @@
-import React, { CSSProperties } from "react";
-
-import background from "../../assets/images/homepage-background.jpg";
-
-const rootStyles: CSSProperties = {
-  minHeight: "200vh",
-  backgroundImage: `url(${background})`,
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-  position: "fixed",
-  top: 0,
-  width: "100%",
-};
+import * as React from "react";
+import Box from "@mui/material/Box";
 
 export default function Landing() {
-  return <div style={rootStyles}></div>;
+  return (
+    <div>
+      <Box
+        sx={{
+          width: 300,
+          height: 1200,
+          background: "none",
+        }}
+      />
+    </div>
+  );
 }
