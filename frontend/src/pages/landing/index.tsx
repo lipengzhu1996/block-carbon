98aabc27d6ad7b97e6167165ad35f860c76c5c02
lipengzhu1996 <lipeng_zhu@outlook.com>
2022-12-03 00:29:51 -0800

    add landing page project briefs

--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -1,5 +1,15 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
+import Collapse from '@mui/material/Collapse';
+import IconButton from "@mui/material/IconButton";
+import Scroll from "@mui/material/Scroll";
+import Visibility from "@mui/icons-material/Visibility";
+
+const styles = {
+  collapse:{
+    textAlign: "center",
+  }
+}
 
 export default function Landing() {
   return (
